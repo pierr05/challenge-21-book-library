6091e3fbbd5921739a2e1dcea74bce2863dd80e0
pierr05 <pierreachlet322@gmail.com>
2023-04-24 11:20:58 -0400

    Added code to server.js file

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
+const { typeDefs, resolvers } = require('');
 const routes = require('./routes');
+// import apolloServer
+const { ApolloServer } = require('apollo-server-express');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -16,6 +24,17 @@ if (process.env.NODE_ENV === 'production') {
 
 app.use(routes);
 
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
+// Create a new instance of an Apollo server with the GraphQL schema
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+  server.applyMiddleware({ app });
+  
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`🌍 Now listening on localhost:${PORT}!`);
+      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    })
+  })
+  };
+
+  startApolloServer(typeDefs, resolvers);

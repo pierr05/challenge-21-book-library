071e4a786f06b8367c671b53ad3ed6d022de46f1
pierr05 <pierreachlet322@gmail.com>
2023-04-27 13:43:17 -0400

    added queries.js file

---
+++ Develop/client/src/utils/queries.js
@@ -0,0 +1,12 @@
+import { gql } from '@apollo/client';
+
+export const GET_ME = gql `
+    query getMe {
+        me {
+            _id
+            userName
+            email
+            bookCount
+        }
+    }
+`; 
